1b8cf0b0ea1917e2df5b2688fefe191832791c12
Abhigyan Prakash Singh <abhigyansingh56@gmail.com>
2023-11-06 16:25:17 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <Form>Hello</Form>
+    </body>
+  );
+}
+function Form(props) {
+  return <form>
+  <h1>Login</h1>
+  <label htmlFor="femail">Enter Your details</label>
+  <input type="email" className="email" placeholder='Email'/>
+  <input type="password" className="password" placeholder='Password'/>
+  <button type="button" >Login</button>
+  <div className="col">
+  {["blue","deepskyblue","red"].map((e)=>{
+    return <Circle color={e} />;
+  })}
+  </div>
+  <span>or Sign Up using</span>
+  <input type="button" value="Sign Up" />
+</form>
+}
+
+function Circle(props) {
+  return (
+    <div style={{height:"30px",width:"30px",borderRadius:"50%",background:props.color}}></div>
   );
 }
 
